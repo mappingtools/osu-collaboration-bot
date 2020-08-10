25a3faf739cae411883580722a4b57db6d88307c
Potoofu <abcdhsud@gmail.com>
2020-08-10 18:10:13 +0200

    guild dir

--- osu-collaboration-bot/Commands/GuildModule.cs
+++ osu-collaboration-bot/Commands/GuildModule.cs
@@ -8,22 +8,25 @@ namespace CollaborationBot.Commands {
     [Group("guild")]
     public class GuildModule :ModuleBase<SocketCommandContext> {
         private readonly CollaborationContext _context;
+        private readonly FileHandlingService _fileHandler;
         private readonly ResourceService _resourceService;
 
-        public GuildModule(CollaborationContext context, ResourceService resourceService) {
+        public GuildModule(CollaborationContext context, FileHandlingService fileHandler, ResourceService resourceService) {
             _context = context;
+            _fileHandler = fileHandler;
             _resourceService = resourceService;
         }
 
         [RequireUserPermission(Discord.GuildPermission.Administrator)]
         [Command("add")]
         public async Task Add() {
-            if( await _context.AddGuild(Context.Guild.Id) ) {
-                await Context.Channel.SendMessageAsync(_resourceService.GenerateAddGuildMessage());
+            if( !await _context.AddGuild(Context.Guild.Id) ) {
+                await Context.Channel.SendMessageAsync(_resourceService.GenerateAddGuildMessage(false));
                 return;
             }
 
-            await Context.Channel.SendMessageAsync(_resourceService.GenerateAddGuildMessage(false));
+            await Context.Channel.SendMessageAsync(_resourceService.GenerateAddGuildMessage());
+            _fileHandler.GenerateGuildDirectory(Context.Guild);
         }
     }
 }
