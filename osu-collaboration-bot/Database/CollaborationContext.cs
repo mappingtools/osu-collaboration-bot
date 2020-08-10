c214362e689f2ccb3856db89969f742d290d04eb
Potoofu <abcdhsud@gmail.com>
2020-08-10 18:34:11 +0200

    create

--- osu-collaboration-bot/Database/CollaborationContext.cs
+++ osu-collaboration-bot/Database/CollaborationContext.cs
@@ -23,7 +23,7 @@ namespace CollaborationBot.Database {
 
         public async Task<bool> AddProject(string name, ulong uniqueGuildId) {
             var id = await ExecuteScalar<int>($"SELECT id FROM Guilds WHERE uniqueGuildId = {uniqueGuildId} LIMIT 1");
-            return await ExecuteNonQuery($"INSERT INTO Projects (name, guildId, status) VALUES('{name}', '{id}', '{ProjectStatus.Not_Started}')") > 0;
+            return await ExecuteNonQuery($"INSERT INTO Projects (name, guildId, status) VALUES('{name}', '{id}', '{(int) ProjectStatus.Not_Started}')") > 0;
         }
 
         public async Task<bool> RemoveProject(string name, ulong uniqueGuildId) {
