38eaa1fcde5bf623fc6e2f991681d5f7707234cb
OliBomby <olivierschipper@gmail.com>
2022-04-28 01:55:51 +0200

    set prefix to be unconfigurable

--- osu-collaboration-bot/Services/AppSettings.cs
+++ osu-collaboration-bot/Services/AppSettings.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CollaborationBot.Services {
+﻿namespace CollaborationBot.Services {
     public class AppSettings
     {
         public string ConnectionString { get; set; }
         public string Token { get; set; }
         public string Path { get; set; }
-        public string Prefix { get; set; }
+        public string Prefix => "/";
     }
 }
