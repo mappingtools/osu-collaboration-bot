33a64fafa2f5752ed55765b42e4f55b67ef629e2
OliBomby <olivierschipper@gmail.com>
2022-05-10 00:50:59 +0200

    a

--- osu-collaboration-bot/Commands/TopModule.cs
+++ osu-collaboration-bot/Commands/TopModule.cs
@@ -26,7 +26,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CollaborationBot.Commands {
-    [Group("", "All common commands")]
+    [Group("mt", "All common commands")]
     public class TopModule : InteractionModuleBase<SocketInteractionContext> {
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
         private static readonly Random random = new();
