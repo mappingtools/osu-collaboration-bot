9999626707bdfd15f700e8c11ccbca5af80d14d9
OliBomby <olivierschipper@gmail.com>
2023-10-03 10:31:37 +0200

    fix naming convention

--- osu-collaboration-bot/Autocomplete/ModuleAutocompleteHandler.cs
+++ osu-collaboration-bot/Autocomplete/ModuleAutocompleteHandler.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace CollaborationBot.Autocomplete {
     public class ModuleAutocompleteHandler : AutocompleteHandler {
-        private static readonly string[] Modules = { "project", "guild", "part", "asn", "au" };
+        private static readonly string[] modules = { "project", "guild", "part", "asn", "au" };
 
         public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction,
             IParameterInfo parameter, IServiceProvider services) {
             var prefix = (string)autocompleteInteraction.Data.Current.Value;
-            return Task.FromResult(AutocompletionResult.FromSuccess(Modules.Where(o => o.StartsWith(prefix)).Select(o => new AutocompleteResult(o, o))));
+            return Task.FromResult(AutocompletionResult.FromSuccess(modules.Where(o => o.StartsWith(prefix)).Select(o => new AutocompleteResult(o, o))));
         }
     }
 }
