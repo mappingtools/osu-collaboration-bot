067ebdf369ef7c7455adf2e04808df11cbe99082
OliBomby <olivierschipper@gmail.com>
2025-08-06 17:29:53 +0200

    clean up input sanitizing service

--- osu-collaboration-bot/Services/InputSanitizingService.cs
+++ osu-collaboration-bot/Services/InputSanitizingService.cs
@@ -3,21 +3,17 @@ using System.Linq;
 
 namespace CollaborationBot.Services {
     public class InputSanitizingService {
-
-        public InputSanitizingService() {
-        }
-
-        private static readonly char[] illegalChars = new char[] { '@', '`' };
+        private static readonly char[] illegalChars = ['@', '`'];
 
         public bool IsValidProjectName(string projectName) {
             return IsValidName(projectName) &&
-                !Path.GetInvalidFileNameChars().Any(o => projectName.Contains(o));
+                !Path.GetInvalidFileNameChars().Any(projectName.Contains);
         }
 
         public bool IsValidName(string name) {
             return !string.IsNullOrWhiteSpace(name) &&
-                (System.Text.Encoding.UTF8.GetByteCount(name) == name.Length) &&
-                !illegalChars.Any(o => name.Contains(o)); ;
+                System.Text.Encoding.UTF8.GetByteCount(name) == name.Length &&
+                !illegalChars.Any(name.Contains);
         }
     }
 }
